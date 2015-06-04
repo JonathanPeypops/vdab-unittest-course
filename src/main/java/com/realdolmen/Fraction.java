45250eaf8cc7667f29df9284f44d09536e48e9f7
Jonathan Peypops <jonathanpeypops@msn.com>
2015-06-04 16:28:29 +0200

    add fraction class

---
+++ src/main/java/com/realdolmen/Fraction.java
@@ -0,0 +1,31 @@
+package com.realdolmen;
+
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public int Numerator(){
+        return numerator;
+    }
+
+    public int Denominator(){
+        return denominator;
+    }
+
+    public void simplify(){
+
+    }
+}
